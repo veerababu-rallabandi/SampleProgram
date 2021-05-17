863f4bf2ecc7ef7a53fb87f3d28e451063663068
veerababu-rallabandi <veerababu.rallabandi@gmail.com>
2021-05-17 13:07:18 +0530

    Methods class added

---
+++ Practice1/Methods.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice1
+{
+    class Methods
+    {
+
+        //without return type & without arguments
+        public void Method1()
+        {
+            Console.WriteLine("I am method1");
+        }
+        //without return type & with paramaters
+        public void Method2(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        //with return type & without parameters
+        public string Method3()
+        {
+            return "hello";
+        }
+        //with return type & with parameters
+        public string Method4(string name)
+        {
+            return "hello " + name;
+        }
+
+        //By using ref keyword
+        public void Method5(ref int a)
+        {
+            a += a;
+        }
+        //By using out keyword
+        public void Method6(out int a)
+        {
+            a = 0;
+            a += 10;
+        }
+        //By using params keyword
+        public void Method7(params object[] a)
+        {
+            foreach(object ele in a)
+            {
+                Console.WriteLine(ele);
+            }
+        }
+        //By using in keyword
+        public void Method8(in int a,ref int b)
+        {
+            b = a + b;
+           
+        }
+    }
+}
--- Practice1/Program.cs
+++ Practice1/Program.cs
@@ -7,6 +7,38 @@ namespace Practice1
         static void Main(string[] args)
         {
             
+            //Methods
+            Methods m = new Methods();
+            m.Method1();
+            m.Method2(10, 20);
+            Console.WriteLine(m.Method3());
+            Console.WriteLine(m.Method4("veeru"));
+            //by using ref keyword
+            int a = 10;
+            Console.WriteLine("befor calling a:"+a);
+            m.Method5(ref a);
+            Console.WriteLine("after calling a:"+a);
+
+            //By using out keyword
+            int b = 10;
+            Console.WriteLine("before calling b:"+b);
+            m.Method6(out b);
+            Console.WriteLine("after calling b:"+b);
+
+            //By using params keyword
+            m.Method7(1,2,3,4);
+            m.Method7(1, "veeru", 1.3f, true);
+
+            //By using in keyword-->read-only
+            int c = 12;
+            int d = 15;
+            Console.WriteLine("before calling c is "+d);
+            Console.WriteLine("before calling d is "+c);
+            m.Method8(c,ref d);
+            Console.WriteLine("after calling d is " +d);
+            Console.WriteLine("after calling c is " +c);
+
+
             StringFormat s = new StringFormat();
           //  s.Method1();
            // s.Method2();
